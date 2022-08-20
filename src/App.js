dfcb82c3455c9f50ce0715ea351df41246708240
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-20 10:51:32 +0530

    Added one modification in the app.js file

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>My Goals for Life</h1>
-     <ol>
-      <li>Job</li>
-      <li>Home</li>
-      <li>Money</li>
-      <li>More Money</li>
-      
-     </ol>
+    <h1>Getting Access from the team lead from GITHUB</h1>
+    
     </div>
   );
 }
