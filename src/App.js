ebdf5f18ee696a3ed93d341d3a047609e88ad823
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-20 10:41:20 +0530

    New src folder
    Modifications done as per the requirement in the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My Goals for Life</h1>
+     <ol>
+      <li>Job</li>
+      <li>Home</li>
+      <li>Money</li>
+      <li>More Money</li>
+      
+     </ol>
+    </div>
+  );
+}
+
+export default App;
